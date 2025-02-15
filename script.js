776808645a3f9fb2a49932dda2e8e3fc921ac2f9
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-02-15 10:46:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 function convertToUppercase() {
-            const inputField = document.getElementById('fname');
-            inputField.value = inputField.value.toUpperCase();
-        }
+          document.getElementById("fname").addEventListener("blur", function() {
+            this.value = this.value.toUpperCase();
+        });
